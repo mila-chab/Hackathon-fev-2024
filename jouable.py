2e4ce7f7d299d3d89c42d6de97266250589a4a49
Nejma Araki <nejma.araki@gmail.com>
2024-02-02 14:41:42 +0100

    marche+joli!!!

--- jouable.py
+++ jouable.py
@@ -88,12 +88,24 @@ def left(t) :
         t[j] = 0
     return t 
 
+def graphicplate(t, screen):
+        font = pygame.font.Font(None, 100)
+        color = (40,58,82)
+        for y in range(SIZE):
+            for x in range(SIZE):
+                if t[x*SIZE + y] != 0:
+                    pygame.draw.rect(screen, color, pygame.Rect(16+x*146, 16+y*146, 142, 142), border_radius=20)
+                    screen.blit( font.render(str(t[x*SIZE + y]),1 ,(255,255,255)) , (70 + 144*x , 58 + 144*y) )
+                    
 #fonction de jeu
 def play() :
     t = taquin #a remplacer par rendu de la fonction random taquin quand elle existera
     running = True
     white = (255, 255, 255)
     blue = (0, 0, 128)
+    green = (9,44,28)
+    marroon = (66,42,100)
+    marroon_light = (200,200,240)
     pygame.init()
     display_surface = pygame.display.set_mode((600, 600))
     font = pygame.font.Font('freesansbold.ttf', 32)
@@ -101,8 +113,11 @@ def play() :
     textRect = text.get_rect()
     textRect.center = (300, 300)
     while running :
-        display_surface.fill(white)
-        display_surface.blit(text, textRect)
+       # display_surface.fill(white)
+       # display_surface.blit(text, textRect)
+       # display_surface.fill( green )
+        pygame.draw.rect(display_surface, marroon, pygame.Rect(10, 10, 585, 585))
+        pygame.draw.rect(display_surface, marroon_light, pygame.Rect(16, 16, 584, 584))
         for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
@@ -115,7 +130,8 @@ def play() :
                         t = right(t)
                     elif event.key == pygame.K_q:
                         running = False
-                text = font.render(string_taquin(t), True, blue, white)
-                pygame.display.update()
+                #text = font.render(string_taquin(t), True, blue, white)
+        graphicplate(t, display_surface)
+        pygame.display.update()
 
     pygame.quit()
